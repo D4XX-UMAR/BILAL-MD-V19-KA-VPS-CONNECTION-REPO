5035caf7ae208f27582c09c4a4a7314f8291c281
UMAR FAROOQ <goodboy78678609@gmail.com>
2026-06-14 23:12:42 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,23 @@
-module.exports = (req, res) => {
-  res.redirect('http://143.198.88.152:10005' + req.url);
+const https = require('https');
+const http = require('http');
+
+module.exports = async (req, res) => {
+  const target = 'http://143.198.88.152:10005';
+  const url = target + req.url;
+
+  const client = url.startsWith('https') ? https : http;
+
+  const proxy = client.request(url, {
+    method: req.method,
+    headers: req.headers
+  }, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res, { end: true });
+  });
+
+  req.pipe(proxy, { end: true });
+
+  proxy.on('error', (err) => {
+    res.status(500).send('Proxy Error: ' + err.message);
+  });
 };
